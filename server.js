dca43739aed864b736f6a7f03da24306802ead9d
ParisaDarkhal <111551848+ParisaDarkhal@users.noreply.github.com>
2023-05-12 17:59:40 -0400

    server.js dependencies and connections set

--- server.js
+++ server.js
@@ -1,23 +1,35 @@
 // Dependencies
-const express = require("express");
-const exphbs = require("express-handlebars");
-const path = require("path");
-const hbs = exphbs.create({});
+const express = require("express"); //
+const exphbs = require("express-handlebars"); //
+const path = require("path"); //
+
+const controllers = require("./controllers");
+const sequelize = require("./config/connection");
+
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express App
 const app = express();
-const PORT = process.env.PORT || 3001;
 
+// to make the tables for the first time
 const models = require("./models");
+
+// sets up handlebars
+const hbs = exphbs.create({});
 
 // Set Handlebars as the default template engine.
 app.engine("handlebars", hbs.engine);
 app.set("view engine", "handlebars");
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(require("./controllers/user-routes"));
+// to make it possible to make a POST request
+app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
+app.use(controllers);
 
-// Starts the server to begin listening
-app.listen(PORT, () => {
-  console.log("Server listening on: http://localhost:" + PORT);
+// Starts the server to begin listening: first we need to connect to the database and then run the server
+sequelize.sync({ force: false }).then(() => {
+  // false can be turned to true ONLY first time when I want to make the database
+  app.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
 });
